a8a6cfeedf250bac3d1f181479509d7393ebe161
Patrick Fox <patrick.fox@razorfish.com>
2016-04-18 23:54:10 -0500

    New deck

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,6 @@
 gulp build
-git push origin `git subtree split --prefix build gh-pages`:gh-pages --force
-git subtree push --prefix build origin gh-pages
-
-
-#git subtree split --prefix output -b gh-pages-temp
-#git push -f origin gh-pages-temp:gh-pages
-#git branch -D gh-pages-temp
+# git push origin `git subtree split --prefix build gh-pages`:gh-pages --force
+# git subtree push --prefix build origin gh-pages
+git subtree split --prefix build -b gh-pages-temp
+git push -f origin gh-pages-temp:gh-pages
+git branch -D gh-pages-temp
