49d4b86f4cc2589d4ee1f72a4ccb6d806ae3b102
Patrick Fox <patrick.fox@razorfish.com>
2017-05-19 10:49:17 -0500

    Updates

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
 gulp build
 # git push origin `git subtree split --prefix build gh-pages`:gh-pages --force
 # git subtree push --prefix build origin gh-pages
+mv .gitignore .gitignore-temp
+cp .gitignore-deploy .gitignore
 git subtree split --prefix build -b gh-pages-temp
 git push -f origin gh-pages-temp:gh-pages
 git branch -D gh-pages-temp
+mv .gitignore-temp .gitignore
